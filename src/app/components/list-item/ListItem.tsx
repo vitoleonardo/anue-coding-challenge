b939e6c5d1563c697ec839f9871e46ea362dcd26
AntoineFachez <anthony.zornig@gmx.de>
2024-11-01 09:47:36 +0100

    create ToDo List, ToDo Item, LS

--- src/app/components/list-item/ListItem.tsx
+++ src/app/components/list-item/ListItem.tsx
@@ -6,7 +6,6 @@ import { Todo } from '@/context/TodoContext';
 interface ListItemProps {
   item: Todo;
 }
-
 const ListItem: React.FC<ListItemProps> = ({ item }) => {
   return (
     <li className="flex flex-row items-center justify-between gap-4 py-2 shadow-custom">
